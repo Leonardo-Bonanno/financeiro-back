40f7fa97641720fae33b5926516b5fe4ed6f5207
Leonardo <leonardobrito2003@gmail.com>
2025-05-11 20:08:41 -0300

    correções para ambiente

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ app.get('/categorias', async (req, res) => {
 });
 
 // Configuração do servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
